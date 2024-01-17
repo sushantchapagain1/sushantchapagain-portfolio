0b8c1dc7555e77fedaecd9e08d1c279c046e6715
sushant chapagain <sreddevil12@gmail.com>
2024-01-17 15:29:48 +0545

    added basic packages for proj

---
+++ components/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { LogoDarkIcon } from './icons';
+
+function Navbar() {
+  return (
+    <header>
+      <LogoDarkIcon height={40} width={40} />
+    </header>
+  );
+}
+
+export default Navbar;
---
+++ data/links.ts
@@ -0,0 +1,14 @@
+export const navlinks = [
+  {
+    label: 'Home',
+    href: '/',
+  },
+  {
+    label: 'Projects',
+    href: '/projects',
+  },
+  {
+    label: 'Blogs',
+    href: '/blogs',
+  },
+];
