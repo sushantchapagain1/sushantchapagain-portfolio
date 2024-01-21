0fa28212fe973c29c5147d626caea56ad6a36083
sushant chapagain <sreddevil12@gmail.com>
2024-01-21 13:14:04 +0545

    styling of not found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,7 +1,20 @@
+import Link from 'next/link';
 import React from 'react';
 
 function NotFound() {
-  return <div>not-found</div>;
+  return (
+    <div className="flex flex-col items-center gap-3 self-center justify-self-center">
+      <p className="font-bold text-foregroundText">
+        404 | The page you are looking could not be found
+      </p>
+      <Link
+        href="/"
+        className="border-b border-foregroundText text-foregroundText"
+      >
+        Go back to homepage
+      </Link>
+    </div>
+  );
 }
 
 export default NotFound;
--- contexts/ThemeContext.tsx
+++ contexts/ThemeContext.tsx
@@ -11,9 +11,8 @@ const ThemeContext = createContext<ThemeContextProps | null>(null);
 
 function ThemeProvider({ children }: { children: React.ReactNode }) {
   const [isDarkMode, setIsDarkMode] = useLocalStorage(
-    (typeof window !== 'undefined' &&
-      window.matchMedia('(prefers-color-scheme:dark)').matches) ||
-      'true', //get system color scheme.
+    typeof window !== 'undefined' &&
+      window.matchMedia('(prefers-color-scheme:dark)').matches, //get system color scheme.
     'isDarkMode',
   );
 
