7a675d26d60d9ee9c2f68af574c419402f80b9f3
sushant chapagain <sreddevil12@gmail.com>
2024-01-30 23:02:38 +0545

    added robots metadata

---
+++ app/robots.ts
@@ -0,0 +1,14 @@
+// This files tells search engines web crawlers(searches automatically on our site content and info)which page or files can or
+// cannot go to request from the site. Usefull to set in admin, some API and more.
+
+import { MetadataRoute } from 'next';
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [{ userAgent: '*', allow: '/', disallow: '/admin-example' }],
+    sitemap: 'sitemap/url',
+    //  its like a helper for web crawlers like  Bing, Yandex, Baidu, Naver, Yahoo or DuckDuckGo) .
+    // if we have 1000 pages then sitemap will force not to overlook other unneeded pages.
+    // https://nextjs.org/docs/app/api-reference/file-conventions/metadata/sitemap for creating a sitemap
+  };
+}
