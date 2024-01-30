660de7ab4ec53f9253f16a0a0cb16e5ad2a080d3
sushant chapagain <sreddevil12@gmail.com>
2024-01-30 18:12:13 +0545

    added open  graph image and somee og metadata

--- data/metadata.ts
+++ data/metadata.ts
@@ -19,6 +19,5 @@ export const siteMetadata = {
     'node js developer',
     'react native developer',
   ],
-  myImgUrl:
-    'https://instagram.fktm18-1.fna.fbcdn.net/v/t51.2885-15/400797270_1142132787145080_9126478657769361185_n.jpg?stp=dst-jpg_e35_p640x640_sh0.08&efg=eyJ2ZW5jb2RlX3RhZyI6ImltYWdlX3VybGdlbi4xNDQweDE4MDAuc2RyIn0&_nc_ht=instagram.fktm18-1.fna.fbcdn.net&_nc_cat=100&_nc_ohc=SNzwv2wBpS0AX_CLsoP&edm=ACWDqb8BAAAA&ccb=7-5&ig_cache_key=MzIzMjYyMzQxNDg3NTk4MDQ1Nw%3D%3D.2-ccb7-5&oh=00_AfDUA79QdqA7a2xD-vCNubTClSPJecuKBXoa54i4VISHcw&oe=65AEB81F&_nc_sid=ee9879',
+  openGraphImage: '/static/favicons/opengraph-image.png',
 };
