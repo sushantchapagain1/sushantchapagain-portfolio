dfd9b9fa83ae4cd9749a3a95c4abc17704129adb
sushant chapagain <sreddevil12@gmail.com>
2024-02-21 22:21:03 +0545

    till use query basic

--- components/SocailIcon.tsx
+++ components/SocailIcon.tsx
@@ -6,12 +6,8 @@ function SocailIcon({
   icon: React.ElementType;
 }) {
   return (
-    <a
-      href={href}
-      target="_blank"
-      className="transition-all duration-300 hover:-translate-y-1"
-    >
-      <Icon className="h-6 w-6 flex-none fill-lightText transition" />
+    <a href={href} target="_blank">
+      <Icon className="h-6 w-6 flex-none fill-lightText transition-all duration-300 hover:-translate-y-1" />
     </a>
   );
 }
